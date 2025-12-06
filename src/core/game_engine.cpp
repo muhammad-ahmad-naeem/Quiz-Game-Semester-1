83496a517f82de193ffb231130b5813018812b3a
Ahmad Naeem <ahmadnaeem.offcl@gmail.com>
2025-12-07 00:47:04 +0500

    Add main menu display function for quiz game

--- src/core/game_engine.cpp
+++ src/core/game_engine.cpp
@@ -1,1 +1,13 @@
 // Game Engine - By Ahmad Naeem
+// Game Engine - By Ahmad Naeem
+#include <iostream>
+#include <string>
+using namespace std;
+
+void displayMainMenu() {
+    cout << "=== QUIZ GAME ===" << endl;
+    cout << "1. Start Quiz" << endl;
+    cout << "2. High Scores" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Choice: ";
+}
